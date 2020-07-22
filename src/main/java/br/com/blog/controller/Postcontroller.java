bb7cf9f086ed279657bb931a1514f23ead13c02d
joaoh <joao_camargo10@hotmail.com>
2020-07-22 18:43:35 -0300

    Controller and html layout

---
+++ src/main/java/br/com/blog/controller/Postcontroller.java
@@ -0,0 +1,26 @@
+package br.com.blog.controller;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.blog.model.Post;
+import br.com.blog.service.PostService;
+
+@Controller
+public class Postcontroller {
+	
+	@Autowired
+	PostService postService;
+	
+	@RequestMapping(value = "/posts", method = RequestMethod.GET)
+	public ModelAndView getPosts() {
+		ModelAndView mv = new ModelAndView("posts");
+		List<Post> posts = postService.findAll();
+		mv.addObject("posts", posts);
+		return mv;
+	}
+}
