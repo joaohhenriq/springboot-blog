98dadb24447c21e8e341c97d48039a39a2a9de9c
joaoh <joao_camargo10@hotmail.com>
2020-07-22 22:03:36 -0300

    Post form

--- src/main/java/br/com/blog/controller/Postcontroller.java
+++ src/main/java/br/com/blog/controller/Postcontroller.java
@@ -1,12 +1,15 @@
 package br.com.blog.controller;
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.blog.model.Post;
 import br.com.blog.service.PostService;
@@ -31,5 +34,26 @@ public class PostController {
 		Post post = postService.findById(id);
 		mv.addObject("post", post);
 		return mv;
+	}
+	
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(Post post, BindingResult result, RedirectAttributes attributes) {
+		
+		if(post.getAuthor().trim().equals("") || post.getText().trim().equals("") || post.getTitle().trim().equals("")) {
+			attributes.addFlashAttribute("message", "You need to fill all the needed fields.");
+			return "redirect:/newpost";
+		}
+		
+		post.setDate(LocalDate.now());
+		
+		postService.save(post);
+		
+		return "redirect:/posts";
 	}
 }
